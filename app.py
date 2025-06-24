9421dbd5084974c60e9551702bc8d017ac496a19
kievdadon <Jameskiev16@icloud.com>
2025-06-24 15:43:52 -0400

    Update app.py

--- app.py
+++ app.py
@@ -120,7 +120,5 @@ def get_locations():
         return jsonify([{"user": r[0], "lat": r[1], "lon": r[2]} for r in c.fetchall()])
 
 # ----- MAIN -----
-if condition:
-    some_function()  # Indented correctly
-
-app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
