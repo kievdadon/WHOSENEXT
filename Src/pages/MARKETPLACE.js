67b894032d75857c4992039ad1ced7705db31bc8
kievdadon <Jameskiev16@icloud.com>
2025-06-24 23:51:01 -0400

    Create MARKETPLACE.js

---
+++ Src/pages/MARKETPLACE.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Marketplace() {
+  return (
+    <div>
+      <h1>WHOSENXT Marketplace 🛒</h1>
+      <p>Here you can post and view items for sale!</p>
+    </div>
+  );
+}
+
+export default Marketplace;
