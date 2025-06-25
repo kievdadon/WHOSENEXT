7c57a164c2ec3275f14be667cf4b52d76bb02e60
kievdadon <Jameskiev16@icloud.com>
2025-06-24 23:55:14 -0400

    Update MARKETPLACE.js

--- Src/pages/MARKETPLACE.js
+++ Src/pages/MARKETPLACE.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React from "react";
 
 function Marketplace() {
   return (
     <div>
       <h1>WHOSENXT Marketplace 🛒</h1>
-      <p>Here you can post and view items for sale!</p>
+      <p>Here you can post and view items for sale, including furniture, clothes, and more!</p>
+      {/* Add functionality to post/view items here */}
     </div>
   );
 }
