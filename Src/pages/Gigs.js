6486d3d3650de080df9dd6a4afed7000fc60a3ad
kievdadon <Jameskiev16@icloud.com>
2025-06-24 23:55:56 -0400

    Update Gigs.js

--- Src/pages/Gigs.js
+++ Src/pages/Gigs.js
@@ -1,1 +1,13 @@
+import React from "react";
 
+function Gigs() {
+  return (
+    <div>
+      <h1>WHOSENXT Gigs 🎯</h1>
+      <p>Find and post gigs here for short-term jobs or tasks!</p>
+      {/* Add functionality to post/apply for gigs here */}
+    </div>
+  );
+}
+
+export default Gigs;
