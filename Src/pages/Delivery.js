c81be6a84a5088d40a9da3ba54ebbf262073c324
kievdadon <Jameskiev16@icloud.com>
2025-06-24 23:56:28 -0400

    Update Delivery.js

--- Src/pages/Delivery.js
+++ Src/pages/Delivery.js
@@ -1,1 +1,13 @@
+import React from "react";
 
+function Delivery() {
+  return (
+    <div>
+      <h1>WHOSENXT Delivery 🚚</h1>
+      <p>Order groceries, meals, or anything else for delivery to your location!</p>
+      {/* Add functionality to request deliveries here */}
+    </div>
+  );
+}
+
+export default Delivery;
