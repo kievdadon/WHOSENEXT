f583025bc79a8084775d7b41c26192f0f66cc844
kievdadon <Jameskiev16@icloud.com>
2025-06-24 23:57:02 -0400

    Update Family.js

--- Src/pages/Family.js
+++ Src/pages/Family.js
@@ -1,1 +1,13 @@
+import React from "react";
 
+function Family() {
+  return (
+    <div>
+      <h1>WHOSENXT Family 👨‍👩‍👧‍👦</h1>
+      <p>Stay connected with your family! Chat, share location, and more.</p>
+      {/* Add functionality for family chat and location sharing here */}
+    </div>
+  );
+}
+
+export default Family;
