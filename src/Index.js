d3eb4dd67c024b660d166d3d0dae179a376dc04c
kievdadon <Jameskiev16@icloud.com>
2025-07-08 16:02:28 -0400

    Update Index.js

--- src/Index.js
+++ src/Index.js
@@ -1,15 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta name="description" content="WHOSENXT App" />
-    <title>WHOSENXT</title>
-  </head>
-  <body>
-    <!-- This is where React will inject your app -->
-    <div id="root"></div>
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-    <!-- If you're using CRA or Vite, the build system injects scripts below -->
-  </body>
-</html>
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
