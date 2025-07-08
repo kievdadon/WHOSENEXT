4952c851cdeda3ed7055c6a54a9bc9f5d5ea0a72
kievdadon <Jameskiev16@icloud.com>
2025-07-08 18:46:27 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,13 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'YOUR_API_KEY',
-  authDomain: 'YOUR_PROJECT.firebaseapp.com',
-  projectId: 'YOUR_PROJECT_ID',
-  storageBucket: 'YOUR_PROJECT.appspot.com',
-  messagingSenderId: 'YOUR_SENDER_ID',
-  appId: 'YOUR_APP_ID',
+  apiKey: "AIzaSyBDN8y6QkjVlPR_9eG5dp0RG4b5iH4EaVg",
+  authDomain: "familygroupchat-f5bb2.firebaseapp.com",
+  projectId: "familygroupchat-f5bb2",
+  storageBucket: "familygroupchat-f5bb2.firebasestorage.app",
+  messagingSenderId: "219263605312",
+  appId: "1:219263605312:web:45ba54b1b6188ecd40dda5",
+  measurementId: "G-7X6Y0VXBKR"
 };
 
 const app = initializeApp(firebaseConfig);
